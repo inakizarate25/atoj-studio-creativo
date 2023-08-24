d5c32532bbd610c9c05786214a4231354e71c406
inakizarate25 <inakizarate25@gmail.com>
2023-08-24 11:54:47 -0300

    arreglos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Services from './components/Services/Services'
 import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import Portfolio from './components/Portfolio/Portfolio'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
@@ -17,9 +18,10 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/servicios" element={<Services/>} />
-        <Route path="/contacto" element={<Contact/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/shop" element={<Shop/>} />
+        <Route path="/portfolio" element={<Portfolio/>} />
         <Route path="*" element={<h2>404 Not found</h2>} />
       </Routes>
       <Footer />
