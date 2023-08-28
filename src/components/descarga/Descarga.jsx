013831560da0f0c21472ce5720e1879013f20dca
inakizarate25 <inakizarate25@gmail.com>
2023-08-28 17:44:49 -0300

    validacion

--- src/components/descarga/Descarga.jsx
+++ src/components/descarga/Descarga.jsx
@@ -4,11 +4,38 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Descarga = () => {
-const [send, setSend] = useState(false)
 
-const handleClick = () => {
-  setSend(true)
-}
+
+  const [send, setSend] = useState(false);
+  const [nombre, setNombre] = useState('');
+  const [email, setEmail] = useState('');
+  const [errores, setErrores] = useState({});
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    // Realiza la validación
+    const newErrores = {};
+    if (!nombre.trim()) {
+      newErrores.nombre = 'El nombre es requerido';
+    }
+    if (!email.trim()) {
+      newErrores.email = 'El email es requerido';
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrores.email = 'Ingrese un email válido';
+    }
+
+    // Si hay errores, no continúes
+    if (Object.keys(newErrores).length > 0) {
+      setErrores(newErrores);
+      return;
+    }
+
+    setErrores({});
+    setSend(true);
+  };
+
+
   return (
     <AnimatedPages>
  <section id="descarga-sec">
@@ -17,23 +44,41 @@ const handleClick = () => {
     <span>{ send ? <div className='msg-desc'>
         <span>¡Que disfrutes tu regalo!</span>
         <p>A través de este <a>LINK</a> podrás obtener tu descargable gratuito</p>
+        <Link className='btn_contact back-home'  to="/">VOLVER AL HOME</Link>
     </div> : ""}</span>
 
     {
-      send ? '' :  <form>
-      <div className="input-group">
-        <label htmlFor="nombre">NOMBRE</label>
-        <input type="text" name="nombre" id="nombre" required/>
-      </div>
-      <div className="input-group">
-        <label htmlFor="mail">MAIL</label>
-        <input type="mail" name="mail" id="mail" required/>
-      </div>
-    </form>
+      send ? '' :  
+      <form>
+              <div className="input-group">
+                <label htmlFor="nombre">NOMBRE</label>
+                <input
+                  type="text"
+                  name="nombre"
+                  id="nombre"
+                  value={nombre}
+                  onChange={(e) => setNombre(e.target.value)}
+                  required
+                />
+                {errores.nombre && <p className="error-msg">{errores.nombre}</p>}
+              </div>
+              <div className="input-group">
+                <label htmlFor="mail">MAIL</label>
+                <input
+                  type="email"
+                  name="mail"
+                  id="mail"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                {errores.email && <p className="error-msg">{errores.email}</p>}
+              </div>
+              <input type="submit" className='btn_contact' value={"CONTINUAR LA COMPRA"} onClick={handleClick} />
+
+            </form>
     }
-    {
-      send ? <Link to='/' className='btn_contact btn-desc'>VOLVER AL HOME</Link> : <button type="submit" className='btn_contact' onClick={handleClick}>DESCARGA GRATUITA</button>
-    }
+ 
     </section>
     </AnimatedPages>
    
