b9e950d6d80e29ab29b6273641467eff1d9ca9e3
Javier <javierscarione@gmail.com>
2023-09-15 13:00:11 -0300

    Javi 15/9 - 13:00

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,24 +9,40 @@ import Separator from "../Separator/Separator"
 // itemdetail
 const Shop = () => {
 
- 
+  const descargablesGratis = descargables.filter((descargable) => descargable.gratis);
+  const descargablesNoGratis = descargables.filter((descargable) => !descargable.gratis);
+
   return (
     <AnimatedPages>
-   <section id='shop'>
+      <section id='shop'>
         <h2>Shop</h2>
         <Separator />
        
 
         <section className="descargables_list">
-        {descargables.map((descargable) => (
-           <article className="descargable" key={descargable.id}>
-           <img src={descargable.img} alt={descargable.title}/>
-           <h3>{descargable.title}</h3>
-           {
-            descargable.gratis ? <Link to={`/descargable/${descargable.id}`} className="descargar">Descargar</Link> : <Link to='/compra' className="descargar">Comprar</Link>
-           }
-       </article>
-           ))}
+        <div className="articulosLista">
+    {descargablesGratis.map((descargable) => (
+      <article className="descargable" key={descargable.id}>
+        <img src={descargable.img} alt={descargable.title} />
+        <h3>{descargable.title}</h3>
+        <Link to={`/descargable/${descargable.id}`} className="descargar">
+          Descargar
+        </Link>
+      </article>
+    ))}
+</div>
+    <h2>Proximamente...</h2>
+<div className="articulosLista venta">
+    {descargablesNoGratis.map((descargable) => (
+      <article className="descargable" key={descargable.id}>
+        <img src={descargable.img} alt={descargable.title} />
+        <h3>{descargable.title}</h3>
+        <Link to="/compra" className="comprar">
+          Comprar
+        </Link>
+      </article>
+    ))}
+    </div>
         </section>
     </section>
     </AnimatedPages>
