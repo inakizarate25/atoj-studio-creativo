23ed1663e6bf3df9654069d6e5ff61829d1e6dce
iñaki zarate <inakizarate25@gmail.com>
2023-09-15 12:49:32 -0300

    scroll

--- src/components/porcentaje/Porcentaje.jsx
+++ src/components/porcentaje/Porcentaje.jsx
@@ -1,35 +1,88 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+// import './styles.css';
+// import PropTypes from 'prop-types';
+
+// const Porcentaje = ({ porcentaje, titulo }) => {
+//   const [valorActual, setValorActual] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       if (valorActual < porcentaje) {
+//         setValorActual(valorActual + 1);
+//       } else {
+//         clearInterval(interval);
+//       }
+//     }, 70);
+
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, [porcentaje, valorActual]);
+
+//   return (
+//     <div className='porcentaje'>
+//       <h4>{titulo}</h4>
+//       <span>{valorActual}%</span>
+//     </div>
+//   );
+// };
+
+// Porcentaje.propTypes = {
+//   porcentaje: PropTypes.number.isRequired, // Cambia el tipo si es necesario
+//   titulo: PropTypes.string.isRequired,
+// };
+
+// export default Porcentaje;
+
+
+import React, { useState, useEffect, useRef } from 'react';
 import './styles.css';
-import PropTypes from 'prop-types';
 
 const Porcentaje = ({ porcentaje, titulo }) => {
-  const [valorActual, setValorActual] = useState(0);
+  const [currentValue, setCurrentValue] = useState(0);
+  const porcentajeRef = useRef(null);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (valorActual < porcentaje) {
-        setValorActual(valorActual + 1);
-      } else {
-        clearInterval(interval);
+    const handleScroll = () => {
+      const element = porcentajeRef.current;
+      const elementTop = element.getBoundingClientRect().top;
+      const elementBottom = element.getBoundingClientRect().bottom;
+      const isVisible = elementTop < window.innerHeight && elementBottom >= 0;
+
+      if (isVisible && currentValue < porcentaje) {
+        const duration = 7000;
+        const steps = porcentaje - currentValue;
+        const stepInterval = duration / steps;
+
+        const interval = setInterval(() => {
+          setCurrentValue((prevValue) =>
+            prevValue + 1 > porcentaje ? porcentaje : prevValue + 1
+          );
+
+          if (currentValue >= porcentaje) {
+            clearInterval(interval);
+          }
+        }, stepInterval);
+
+        return () => {
+          clearInterval(interval);
+        };
       }
-    }, 40);
+    };
+
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      clearInterval(interval);
+      window.removeEventListener('scroll', handleScroll);
     };
-  }, [porcentaje, valorActual]);
+  }, [porcentaje, currentValue]);
 
   return (
-    <div className='porcentaje'>
+    <div className='porcentaje' ref={porcentajeRef}>
       <h4>{titulo}</h4>
-      <span>{valorActual}%</span>
+      <span>{currentValue}%</span>
     </div>
   );
-};
-
-Porcentaje.propTypes = {
-  porcentaje: PropTypes.number.isRequired, // Cambia el tipo si es necesario
-  titulo: PropTypes.string.isRequired,
 };
 
 export default Porcentaje;
